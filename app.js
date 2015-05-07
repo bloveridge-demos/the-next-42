29ebaf57135fbfbda07b584f8e7e52f0d873baf4
Ben Loveridge <benl@gradecam.com>
2015-05-06 21:51:25 -0600

    Step 2: added basic directive to polyfill basic autofocus behavior

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 /* global angular */
 var app = angular.module('demo', []);
+
+app.directive('autofocus', function($timeout) {
+    return {
+        restrict: 'A',
+        link: function(scope, elem, attrs) {
+            $timeout(function() {
+                elem[0].focus();
+            }, 0, false);
+        }
+    };
+});
