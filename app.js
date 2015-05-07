bf6191239ee84012314c9436c8a5ac90f0b052e8
Ben Loveridge <benl@gradecam.com>
2015-05-06 22:12:05 -0600

    Step 3: added support for autofocus upon a condition becoming true

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ app.directive('autofocus', function($timeout) {
     return {
         restrict: 'A',
         link: function(scope, elem, attrs) {
-            $timeout(function() {
-                elem[0].focus();
-            }, 0, false);
+            if (!attrs.autofocus) {
+                $timeout(function() {
+                    elem[0].focus();
+                }, 0, false);
+                return;
+            }
+            scope.$watch(attrs.autofocus, function(condition) {
+                if (condition === true) {
+                    elem[0].focus();
+                }
+            });
         }
     };
 });
