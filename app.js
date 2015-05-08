933730c305d08302554fcfebc8c11feeedb37d8a
Ben Loveridge <benl@gradecam.com>
2015-05-07 20:45:18 -0600

    Step 3: teach directive how to delete books

--- app.js
+++ app.js
@@ -8,14 +8,27 @@ app.controller('BooksCtrl', function() {
         {title: "Pudd'nhead Wilson", author: "Mark Twain"},
         {title: "A Christmas Carol", author: "Charles Dickens"}
     ];
+
+    this.deleteBook = function(book) {
+        this.list.splice(this.list.indexOf(book), 1);
+    };
 });
 
 
 app.directive('bookList', function() {
     return {
         templateUrl: 'bookList.tpl.html',
-        scope: {
+        scope: true,
+        bindToController: {
             books: '=',
+            deleteFn: '&delete'
+        },
+        controllerAs: 'bookList',
+        controller: function($attrs) {
+            this.canDelete = !!$attrs['delete'];
+            this.callDeleteFn = function(book) {
+                this.deleteFn({book:book});
+            };
         }
     };
 });
